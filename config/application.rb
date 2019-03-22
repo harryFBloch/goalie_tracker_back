10a56340163854edefe5808815e3ab0363b184cf
harryFBloch <harryfbloch@gmail.com>
2019-03-22 16:01:35 -0400

    updated Cors origins policy for heroku site

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module GoalieTrackerBack
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001'
+        origins 'https://goalie-tracker-front.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
